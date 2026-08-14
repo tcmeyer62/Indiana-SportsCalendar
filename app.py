103ae40850a3364459f6b7b575a84a6499066468
tcmeyer62 <tcmeyer62@gmail.com>
2026-08-14 20:26:56 +0000

    Version 0.2 - Connect calendar to schedule data

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
+CSV_FILE = "data/data/master/master_schedule.csv"
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    schedule = []
+
+    with open(CSV_FILE, newline="", encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        schedule = list(reader)
+
+    return render_template("index.html", schedule=schedule)
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True).gitignore
-    
+    app.run(host="0.0.0.0", port=5001, debug=True)
